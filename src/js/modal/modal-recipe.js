34029c2098a32a81518d34358713f9c8d04f64a4
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-06-25 18:34:15 +0300

    sum

--- src/js/modal/modal-recipe.js
+++ src/js/modal/modal-recipe.js
@@ -3,7 +3,7 @@ const refs = {
   name: document.getElementById('name-reciepe'),
   rating: document.getElementById('rating'),
   time: document.getElementById('time'),
-  img: document.getElementById('image'),
+  //   img: document.getElementById('image'),
   ingredients: document.getElementById('ingredients'),
   tags: document.getElementById('tags'),
   instructions: document.getElementById('instructions'),
@@ -13,7 +13,7 @@ async function getReciepeById(id) {
   refs.name.textContent = reciepe.title;
   refs.rating.textContent = reciepe.rating;
   refs.time.textContent = reciepe.time;
-  refs.img.textContent = reciepe.thumb;
+  //   refs.img.textContent = reciepe.thumb;
   refs.ingredients.textContent = reciepe.ingredients;
   refs.tags.textContent = reciepe.tags;
   refs.instructions.textContent = reciepe.instructions;
