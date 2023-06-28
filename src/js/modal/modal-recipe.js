54bbd509a18ff9eca5db66f12f2db49e0a3b7484
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-06-28 23:00:35 +0300

    sum

--- src/js/modal/modal-recipe.js
+++ src/js/modal/modal-recipe.js
@@ -1,7 +1,8 @@
 import { getRecipe } from '../service/api.js';
+import { addFavorit, loadFavorit } from '../service/localstorage';
 
 const refs = {
-  modal: document.querySelector('[data-modal]'),
+  modal: document.querySelector('[data-modal-reciepe]'),
   name: document.getElementById('name-reciepe'),
   rating: document.getElementById('rating'),
   time: document.getElementById('time'),
@@ -9,15 +10,18 @@ const refs = {
   ingredients: document.getElementById('ingredients'),
   tags: document.getElementById('tags'),
   instructions: document.getElementById('instructions'),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
+  closeModalBtn: document.querySelector('[data-modal-close-btn]'),
   addButton: document.getElementById('add-favorite'),
+  list: document.querySelector('.cards-list'),
 };
 
 async function getReciepeById(id) {
   const reciepe = await getRecipe(id);
+
   refs.name.textContent = reciepe.title;
   refs.rating.textContent = reciepe.rating;
   refs.time.textContent = `${reciepe.time} min`;
+  refs.addButton.setAttribute('data-recipe-id', id);
   refs.media.insertAdjacentHTML(
     'beforeend',
     createIngredientMedia(reciepe.youtube, reciepe.thumb, reciepe.title)
@@ -65,6 +69,13 @@ function createIngredientMedia(youtubeLink, imageLink, alt) {
     )}"></iframe>`;
   }
 }
+refs.list.addEventListener('click', openModal);
+function openModal(event) {
+  if (event.target.hasAttribute('data-id')) {
+    console.log(event.target);
+    getReciepeById(event.target.getAttribute('data-id'));
+  }
+}
 
 (() => {
   refs.closeModalBtn.addEventListener('click', toggleModal);
@@ -72,3 +83,21 @@ function createIngredientMedia(youtubeLink, imageLink, alt) {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+refs.addButton.addEventListener('click', fetchRecipes);
+async function fetchRecipes() {
+  try {
+    const id = refs.addButton.getAttribute('data-recipe-id');
+    const reciepe = await getRecipe(id);
+    const recipeData = await getDataRecipe(reciepe);
+    addFavorit(recipeData);
+    console.log(loadFavorit());
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function getDataRecipe(data) {
+  const { _id, category, preview, title, description, rating } = data;
+  return { _id, category, preview, title, description, rating };
+}
