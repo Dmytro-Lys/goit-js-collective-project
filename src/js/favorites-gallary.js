41c3c0d8d9c1ff7de69cd67f579a82b29c0b338e
NesterovSerhii <sergei_nesterov8@ukr.net>
2023-06-28 06:20:49 +0300

    Summary

--- src/js/favorites-gallary.js
+++ src/js/favorites-gallary.js
@@ -1,8 +1,13 @@
+import { saveData, loadData,  addFavorit, loadFavorit } from './service/localstorage.js'
 
 const categoryBtns = document.querySelectorAll('.category-btn');
-const heroImage = document.querySelector('.favorites-hero')
+const heroImage = document.querySelector('.favorites-hero');
+const categoriesDiv = document.querySelector('.categories');
+const favBtnList = document.querySelector('.categories-list');
+const noFavoritesEl = document.querySelector('.no-favorites');
+const savedData = loadData('cardData');
 
-window.addEventListener('DOMContentLoaded', hideCategories)
+window.addEventListener('DOMContentLoaded', hideCategories);
 
 function hideCategories() {
     if (localStorage.length !== 0) {
@@ -10,10 +15,11 @@ function hideCategories() {
     }
     else {
         categoryBtns.forEach(btn => {
-            btn.classList.add('invisible')
+            btn.classList.add('invisible');
         });
     }
 }
+// функция сверху не работает 
 
 function hideHeroImageLess768() {
     const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
@@ -24,6 +30,22 @@ function hideHeroImageLess768() {
         heroImage.classList.remove('invisible');
     }
 }
+hideCategories();
+hideHeroImageLess768();
 
 window.addEventListener("load", hideHeroImageLess768);
 window.addEventListener("resize", hideHeroImageLess768);
+
+
+
+function markup() {
+    const noFavoritesEl = document.querySelector('.no-favorites'); 
+    const savedData = localStorage.getItem('cardData');
+    if (savedData) {
+           noFavoritesEl.style.display = "none" 
+    }
+    else {
+        noFavoritesEl.style.display = "flex" 
+    }
+}
+markup();
